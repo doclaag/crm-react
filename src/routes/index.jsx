4a5ede4dc47be6f735a56f02c22704b0c346fdf2
Luis Alonzo <doclaag@gmail.com>
2023-11-16 12:25:49 -0600

    created routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,8 +1,36 @@
 import { createBrowserRouter } from 'react-router-dom';
+import { ErrorPage, Layout } from '../components';
+import { Home, Loader as clientsLoader } from '../pages';
+import { NewClient, action as newClientAction } from '../pages/NewClient';
+import { EditClient, loader as editClientLoader, action as editClientAction } from '../pages/EditClient';
+import { action as deleteClientAction } from '../components/Client';
 
 export const router = createBrowserRouter([
     {
         path: '/',
-        element: <h1>Inicio</h1>
+        element: <Layout />,
+        children: [
+            {
+                index: true,
+                element: <Home />,
+                loader: clientsLoader,
+                errorElement: <ErrorPage />
+            },
+            {
+                path: '/clientes/nuevo',
+                element: <NewClient />,
+                action: newClientAction,
+                errorElement: <ErrorPage />
+            }, {
+                path: '/clientes/:clientId/editar',
+                element: <EditClient />,
+                loader: editClientLoader,
+                action: editClientAction,
+                errorElement: <ErrorPage />
+            }, {
+                path: '/clientes/:clientId/eliminar',
+                action: deleteClientAction
+            },
+        ],
     },
 ]);
